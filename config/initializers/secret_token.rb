c860d330cbe9b74cd9074ffa5bfec73e1112060a
mskubenich <misha.skubenich@gmail.com>
2013-12-05 12:43:03 +0200

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trucking::Application.config.secret_token = 'ff8966852c301e9ee07872ad79a0c1f1a698d376edaf148a25cf969318c08959e3d76ef1666c352e9ef79bedddd7e1ad98e3a32520c6c990b93717ba3a83097b'
