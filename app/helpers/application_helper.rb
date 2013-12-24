402486eecc2053f8923979da6f770ae56be89878
mskubenich <misha.skubenich@gmail.com>
2013-12-24 14:23:20 +0200

    move language_select to partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,16 @@ module ApplicationHelper
   def collaborators_list
     Admin::Collaborator.all
   end
+
+  def locales
+    [
+        {value: 'en', flag: 'gb', title: 'Great Britain (UK)'},
+        {value: 'de', flag: 'de', title: 'Germany'},
+        {value: 'ru', flag: 'ru', title: 'Russian Federation'},
+        {value: 'ua', flag: 'ua', title: 'Ukraine'},
+        {value: 'sk', flag: 'sk', title: 'Slovakia'},
+        {value: 'cz', flag: 'cz', title: 'Czech Republic '},
+        {value: 'pl', flag: 'pl', title: 'Poland '}
+    ]
+  end
 end
