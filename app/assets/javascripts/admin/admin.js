38bb3f6304807bee2bba117b2eaa5c328f2be879
mskubenich <misha.skubenich@gmail.com>
2013-12-24 15:05:35 +0200

    country select in admin

--- app/assets/javascripts/admin/admin.js
+++ app/assets/javascripts/admin/admin.js
@@ -3,3 +3,11 @@
 //= require twitter/bootstrap
 //= require ckeditor/init
 //= require bootstrap_file_upload/bootstrap-fileupload.min
+//= require country_select/js/msdropdown/jquery.dd.min
+
+$(document).ready(function() {
+    $("#countries").msDropdown();
+    $("#countries").change(function(){
+        window.location.href = $(this).find('option:selected').data('href');
+    });
+});
