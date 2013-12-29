0667ce6ce124aed217a1ca4449cc3326a774d9d4
mskubenich <misha.skubenich@gmail.com>
2013-12-29 22:07:13 +0200

    fix cufon fonts

--- app/assets/javascripts/cufon-replace.js
+++ app/assets/javascripts/cufon-replace.js
@@ -1,11 +1,3 @@
-Cufon.replace('#header .row-2 ul li', { fontFamily: 'CalibriR', textShadow:'#397ece 1px 1px' });
 Cufon.replace('h2 b', { fontFamily: 'CalibriB', color: '-linear-gradient(#3b98dd, #3b98dd, #3b98dd, #3b98dd, #3b98dd, #3b98dd, #1f7dd2, #1f7dd2, #1f7dd2, #1f7dd2, #1f7dd2)' });
-Cufon.replace('#search-form label', { fontFamily: 'CalibriI' });
-Cufon.replace('.link1', { fontFamily: 'CalibriI', textShadow:'#165ebd 1px 1px' });
-Cufon.replace('h3, h4', { fontFamily: 'CalibriB' });
-Cufon.replace('h3 b, h4 b', { fontFamily: 'CalibriB' });
-Cufon.replace('.box2 h4, .box3 h4', { fontFamily: 'CalibriB', textShadow:'#2074cb 1px 1px' });
-
-
-Cufon.replace('.calibri', { fontFamily: 'CalibriB', textShadow:'#1f7dd2 1px 1px' });
-Cufon.replace('.calibri b', { fontFamily: 'CalibriB', textShadow:'#ffffff 1px 1px' });
+Cufon.replace('.calibri', { fontFamily: 'CalibriB' });
+Cufon.replace('.calibri b', { fontFamily: 'CalibriB' });
