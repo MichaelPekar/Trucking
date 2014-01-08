f895af68165798c002c5bb77ff2dafcb24cab8c5
mskubenich <misha.skubenich@gmail.com>
2014-01-08 15:18:40 +0200

    add logo

--- app/assets/javascripts/cufon-replace.js
+++ app/assets/javascripts/cufon-replace.js
@@ -1,3 +1,3 @@
-Cufon.replace('h2 b', { fontFamily: 'CalibriB', color: '-linear-gradient(#3b98dd, #3b98dd, #3b98dd, #3b98dd, #3b98dd, #3b98dd, #1f7dd2, #1f7dd2, #1f7dd2, #1f7dd2, #1f7dd2)' });
+Cufon.replace('h3 b', { fontFamily: 'CalibriB', color: '-linear-gradient(#3b98dd, #3b98dd, #3b98dd, #3b98dd, #3b98dd, #3b98dd, #1f7dd2, #1f7dd2, #1f7dd2, #1f7dd2, #1f7dd2)' });
 Cufon.replace('.calibri', { fontFamily: 'CalibriB' });
 Cufon.replace('.calibri b', { fontFamily: 'CalibriB' });
