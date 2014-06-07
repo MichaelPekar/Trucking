8bd05ba35de1475254cbba3da6985541ce2d6c85
misha <misha.skubenich@gmail.com>
2014-06-07 16:43:02 +0300

    design for order email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,6 +2,7 @@ class OrderMailer < ActionMailer::Base
   default from: "Administrator <#{ Admin::MailerSettings.instance.email }>"
   def accept_order(order)
   @order = order
+    attachments.inline['logo.png'] = File.read("#{Rails.root}/app/assets/images/logo.png")
     mail :to => order.email, :subject => "BABYDORYCH-TRANS.COM.UA. Your order is accepted."
   end
 
