87e661755b2e17f840c6d607f9a9214b9a226c2c
misha <misha.skubenich@gmail.com>
2014-06-14 17:22:24 +0300

    increase gallery item size

--- app/models/admin/gallery_item.rb
+++ app/models/admin/gallery_item.rb
@@ -2,7 +2,7 @@ class Admin::GalleryItem < ActiveRecord::Base
   attr_accessible :cz_text, :de_text, :en_text, :pl_text, :ru_text, :sk_text, :ua_text, :image
 
   has_attached_file :image, :url => "/attachments/gallery_item_images/:attachment/:class/:id/:style_:basename.:extension",
-                    :styles => { :original => "481x356#" },
+                    :styles => { :original => "1200x380#" },
                     :default_url => "missing.png"
 
   def text
