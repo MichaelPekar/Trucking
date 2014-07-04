c6c677d3da7463a063e9250b6781b7788500d9be
misha <misha.skubenich@gmail.com>
2014-07-04 19:45:26 +0300

    fix locale for services

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
   def index
     @partners = Admin::Partner.all
-    @sections = Admin::PageSection.where(lang: params[:locale])
+    @sections = Admin::PageSection.where(lang: I18n.locale)
   end
 
 end
